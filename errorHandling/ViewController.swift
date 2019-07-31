d6930deb614c0d532dc1feeb41b1fa7df3bc91e9
Brendan Milton <mirrorimage101@hotmail.com>
2019-07-31 10:52:25 +0100

    Initial Commit

---
+++ errorHandling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  errorHandling
+//
+//  Created by Brendan Milton on 31/07/2019.
+//  Copyright © 2019 Brendan Milton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
